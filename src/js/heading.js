6545ffb8fd796a3f63dc7115e6a4ee46dfea17fa
Jonathan Silva <silva.jon86@gmail.com>
2016-08-21 12:47:44 -0400

    finished routes on main.js for heading, and started jsx on heading.js

---
+++ src/js/heading.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute, Link } from 'react-router';
+
+export default class Heading extends Component {
+	render() {
+		return (
+			<div>
+				<div className="heading">
+					
+				<div>
+			</div>
+		)
+	}
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import Heading from './heading';
 import Home from './home';
@@ -11,3 +11,14 @@ import Login from './login';
 import Dashboard from './dashboard';
 import CreateNewListing from './createNewListing';
 
+ReactDOM.render ((
+	<Router history={hashHistory}>
+		<Route path="/" component={Heading}>
+			<IndexRoute component={Main} />
+			<Route path="/home" component={Home} />
+			<Route path="/inventory" component={Inventory} />
+			<Route path="/about" component={About} />
+			<Route path="/contact" component={Contact} />
+		</Route>
+	</Router>
+	), document.querySelector('.container'));
