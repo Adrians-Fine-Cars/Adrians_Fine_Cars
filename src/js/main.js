ded76a28e1680a887f34eed135337b5f448258c8
Jonathan Silva <silva.jon86@gmail.com>
2016-08-21 12:14:57 -0400

    Added imports to main.js file

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import Heading from './heading';
+import Home from './home';
+import Inventory from './inventory';
+import VehicleOverview from './vehicle_overview';
+import About from './about';
+import Contact from './contact';
+import Login from './login';
+import Dashboard from './dashboard';
+import CreateNewListing from './createNewListing';
+
