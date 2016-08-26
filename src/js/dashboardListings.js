7f1c532c51e2fd5c1fad3eb3722cd7c6cd0ed41f
Jonathan Silva <silva.jon86@gmail.com>
2016-08-26 12:15:39 -0400

    set up ajax on the dashboard listings

--- src/js/dashboardListings.js
+++ src/js/dashboardListings.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHitory, IndexRoute, Link } from 'react-router';
+import { ajax } from 'jquery';
 
 
 export default class DashboardListings extends Component {
+	constructor(...args){
+		super(...args);
+		this.state = { vehicles: [] }
+	}
+
+	componentWillMount() {
+		ajax({
+			url: 'https://adrians-fine-cars-server.herokuapp.com/vehicles/',
+			type: 'GET',
+			cache: false,
+			dataType: 'json'
+		}).then( response => {
+			this.setState( {vehicles: response} );
+		});
+	}
+
+	createResults(vehicles) {
+
+		return (
+			<div key={ vehicles.id }>
+				<div>{ vehicles.makemodel }</div>
+				<div>{ vehicles.price }</div>
+			</div>
+		)
+	}
+
+
 	render() {
+		let { vehicles } = this.state;
+		console.log(vehicles);
 		return (
 			<div className="dashboard_listings_wrapper">
-				Dashboard Vehicle Listings...
+				{ this.state.vehicles.map(::this.createResults) }
 			</div>
 		)
 	}
