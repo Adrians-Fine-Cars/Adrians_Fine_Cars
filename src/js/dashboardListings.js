883d32b5998c6315c0dd68687d9a431e90db44e8
Jonathan Silva <silva.jon86@gmail.com>
2016-09-02 20:33:01 -0400

    Added delete function to dashboardListings.js

--- src/js/dashboardListings.js
+++ src/js/dashboardListings.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHitory, IndexRoute, Link } from 'react-router';
 import { ajax } from 'jquery';
+import Cookies from 'js-cookie';
 
 export default class DashboardListings extends Component {
 	constructor(...args){
@@ -20,12 +21,28 @@ export default class DashboardListings extends Component {
 		});
 	}
 
+	deleteHandler(vehicle) {
+		ajax({
+			url: `https://adrians-fine-cars-server.herokuapp.com/vehicles/${vehicle.id}`,
+			type: 'DELETE',
+			headers: {
+				Authorization: `Bearer ${Cookies.getJSON("user").user.access_token}`
+			}
+		}).then (response => {
+			console.log('response', response);
+			let { vehicles } = this.state;
+			vehicles.splice(vehicles.indexOf(vehicles), 1);
+			this.setState({vehicles: vehicles});
+		})
+	}
+
 	createResults(vehicles) {
 
 		return (
 			<div key={ vehicles.id }>
 				<div>{ vehicles.makemodel }</div>
 				<div>{ vehicles.price }</div>
+				<button onClick={ this.deleteHandler.bind(this, vehicles)}>Delete</button>
 			</div>
 		)
 	}
