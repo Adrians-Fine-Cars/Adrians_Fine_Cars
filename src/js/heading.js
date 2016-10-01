7a5d9537963dea17682c60adb92648944caf3985
Jonathan Silva <silva.jon86@gmail.com>
2016-10-01 19:35:30 -0400

    Adding SASS elements to the Header section

--- src/js/heading.js
+++ src/js/heading.js
@@ -6,14 +6,21 @@ export default class Heading extends Component {
 	render() {
 		return (
 			<div>
-				<div className="heading">
-					<Link to="/"><div className="site-logo"></div></Link>
-					<Link className="heading-link" to="/"> Home </Link>
-					<Link className="heading-link" to="/inventory"> Inventory </Link>
-					<Link className="heading-link" to="/about"> About </Link>
-					<Link className="heading-link" to="/contact"> Contact </Link>
-					<span className="phone-number">(678)850-0765</span>
+				<div className="nav-header w-nav" data-animation="default" data-collapse="tiny" data-contain="1" data-duration="400">
+					<div className="nav-container w-container">
+						<Link to="/"><div className="brand w-nav-brand"><img className="nav-logo-image" src="images/AFC_Logo.png"/></div></Link>
+						<nav className="nav-menu w-nav-menu" role="navigation">
+							<Link className="nav-link w-nav-link" to="/"> Featured Vehicles </Link>
+							<Link className="nav-link w-nav-link" to="/inventory"> Inventory </Link>
+							<Link className="nav-link w-nav-link" to="/about"> About </Link>
+							<Link className="nav-link w-nav-link" to="/contact"> Contact </Link>
+							<div className="phone-wrapper"><img className="phone-icon" sizes="(max-width: 479px) 100vw, 23px" src="images/phone-icon.png" srcsSet="images/phone-icon-p-500x501.png 500w, images/phone-icon.png 594w" width="23"/>
+							<a className="nav-phone-number" href="tel:6788500765">(678) 850-0765</a></div>
+						</nav>
+					</div>
 				</div>
+
+
 				{this.props.children}
 				<footer className="footer">
 					<div>Adrians Fine Cars 2016</div>
